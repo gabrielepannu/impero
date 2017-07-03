4399307d30d7e9b6c29587cbf24bef9a6e36577d
gabrielepannu <eureomain@gmail.com>
2017-07-03 18:08:21 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'impero',
+		wp_ws_url : 'http://www.imperodelbenessere.it/wp-appkit-api/impero',
+		wp_url : 'http://www.imperodelbenessere.it',
+		theme : 'q-android',
+		version : '0.1',
+		app_title : 'Impero',
+		app_platform : 'android',
+		gmt_offset : 2,
+		debug_mode : 'off',
+		auth_key : 'jjut.gW^j_@K  TQ <,oQ%vfSNUi<&!cF=Z.+Tw%R{HhIjew-,$#rxFdd)raw(Vv',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
